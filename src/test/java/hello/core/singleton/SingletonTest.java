b8ea5a02b6e903c964f43ab44f55b672a4435a81
HoChoRoo <rttkhc0304@gmail.com>
2024-08-20 20:32:39 +0900

    싱글톤 주의점

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -45,7 +45,6 @@ public class SingletonTest {
     @Test
     @DisplayName("스프링 컨테이너와 싱글톤")
     void springContainer(){
-//        AppConfig appConfig = new AppConfig();
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
 
