85238c8a15efbc516f3c91147a1f716b6fbe5dd1
HoChoRoo <rttkhc0304@gmail.com>
2024-08-27 20:54:55 +0900

    웹 스코프, 프록시

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.FilterType;
 public class AutoAppConfig {
     
 
-    @Bean(name = "memoryMemberRepository")
+    //@Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,8 +2,10 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -12,59 +14,45 @@ import static org.assertj.core.api.Assertions.*;
 
 public class SingletonWithPrototypeTest1 {
     @Test
-    void prototypeFind() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        prototypeBean1.addCount();
-        assertThat(prototypeBean1.getCount()).isEqualTo(1);
-
-        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
-        prototypeBean2.addCount();
-        assertThat(prototypeBean2.getCount()).isEqualTo(1);
-
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new
+                AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
     }
-
-    @Test
-    void singletonClientUsePrototype(){
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-
-    }
-
-    @Scope("singleton")
-    static class ClientBean{
-        private final PrototypeBean prototypeBean;
+    static class ClientBean {
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
-        public int logic(){
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
-            return prototypeBean.getCount();
+            int count = prototypeBean.getCount();
+            return count;
         }
     }
-
     @Scope("prototype")
     static class PrototypeBean {
         private int count = 0;
-
         public void addCount() {
             count++;
         }
-
         public int getCount() {
             return count;
         }
-
         @PostConstruct
         public void init() {
-            System.out.println("PrototypeBean.init" + this);
+            System.out.println("PrototypeBean.init " + this);
         }
-
         @PreDestroy
         public void destroy() {
-            System.out.println("PrototypeBean.destroy" + this);
+            System.out.println("PrototypeBean.destroy");
         }
     }
+
 }
